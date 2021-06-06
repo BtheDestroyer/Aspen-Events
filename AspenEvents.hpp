fe8bcd6ba9e1f57c86b8c50bf2d0524e8ff21dfb
Bryce Dixon <pokemonbrycedixon@gmail.com>
2021-06-06 01:34:16 -0700

    Added auto-build of aggregate header

--- AspenEvents.hpp
+++ AspenEvents.hpp
@@ -1,2 +1,12 @@
 #pragma once
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This is an aggregate header file for the Events module of Aspen
+// Include it to include every header file in that module
+// However, this does *not* include all library header files
+// You will also need to tell your compiler to treat the parent directory as an additional include directory
+// Finally, make sure you also link against AspenEvents.lib
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "Events\AspenEvents.hpp"
+#include "Events\framework.hpp"
+#include "Events\pch.hpp"
+#include "Events\Event\Event.h"
